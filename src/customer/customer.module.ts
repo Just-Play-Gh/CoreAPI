879a4b0c44b76f56e508ff50d7cafd8f57c6a883
David Yartey <20254688+dneey@users.noreply.github.com>
2021-11-26 00:45:13 +0000

    customer ratings summary

--- src/customer/customer.module.ts
+++ src/customer/customer.module.ts
@@ -3,11 +3,12 @@ import { AuthModule } from './auth/auth.module';
 import { CustomerController } from './customer.controller';
 import { CustomerService } from './customer.service';
 import { RatingsModule } from './ratings/ratings.module';
+import { RatingsSummaryModule } from './ratings-summary/ratings-summary.module';
 
 @Module({
   controllers: [CustomerController],
   exports: [CustomerService],
   providers: [CustomerService],
-  imports: [AuthModule, CustomerModule, RatingsModule],
+  imports: [AuthModule, CustomerModule, RatingsModule, RatingsSummaryModule],
 })
 export class CustomerModule {}
