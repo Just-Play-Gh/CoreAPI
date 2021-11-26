b5bcf64f883867cba7c86dd66fd27f73cc62ca11
David Yartey <20254688+dneey@users.noreply.github.com>
2021-11-26 00:00:41 +0000

    ratings entity

--- src/customer/customer.module.ts
+++ src/customer/customer.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import { RatingsModule } from './ratings/ratings.module';
 
 @Module({
-  imports: [AuthModule, UserModule],
+  imports: [AuthModule, UserModule, RatingsModule],
   providers: [],
 })
 export class CustomerModule {}
