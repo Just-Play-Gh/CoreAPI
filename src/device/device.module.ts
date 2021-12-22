961b3f971838735d80e917f24543f3ebed516ddf
Komla Sodji <duhamelgking@gmail.com>
2021-12-22 18:19:33 +0000

    update service

--- src/device/device.module.ts
+++ src/device/device.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { DeviceService } from './device.service';
 import { DeviceController } from './device.controller';
+import { SharedModule } from 'src/shared/shared.module';
 
 @Module({
+  imports: [SharedModule],
   controllers: [DeviceController],
-  providers: [DeviceService]
+  providers: [DeviceService],
 })
 export class DeviceModule {}
