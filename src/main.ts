6dd19ce897e9e932d8e7072c8e5010e4f0a62a3c
David Yartey <20254688+dneey@users.noreply.github.com>
2021-12-24 21:29:24 +0000

    enable cors and csrf updates

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import cookieParser from 'cookie-parser';
 import helmet from 'helmet';
-import * as csurf from 'csurf';
+import csurf from 'csurf';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(helmet());
-  app.use(csurf());
+  // app.use(csurf());
   app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
   app.setGlobalPrefix('/api/v1');
   await app.listen(process.env.PORT);
 }
