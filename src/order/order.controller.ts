d535f95d138e6c6d8e14f35d882a80bab04a6bc6
David Yartey <20254688+dneey@users.noreply.github.com>
2021-12-28 15:02:33 +0000

    cancel order, get order logs

--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -12,6 +12,7 @@ import { JwtAuthGuard } from 'src/guards/jwt-auth.guard';
 import { BaseController } from 'src/resources/base.controller';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { UpdateOrderDto } from './dto/update-order.dto';
+import { OrderLog } from './entities/order-logs.entity';
 import { Order } from './entities/order.entity';
 import { OrderService } from './order.service';
 
@@ -28,11 +29,26 @@ export class OrderController extends BaseController {
   }
 
   // Should have permission to accept or role
+  @UseGuards(JwtAuthGuard)
   @Get(':id/accept')
   async acceptOrder(
     @CurrentUser() driver,
     @Param() id: string,
   ): Promise<Order> {
     return this.orderService.acceptOrder(driver.id, id);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Patch(':id/cancel')
+  async cancelOrder(
+    @CurrentUser() customer,
+    @Param() id: string,
+  ): Promise<Order> {
+    return this.orderService.cancelOrder(id, customer);
+  }
+
+  @Get(':id/logs')
+  async getOrderLogs(@Param() orderId: number): Promise<OrderLog[]> {
+    return await this.orderService.getOrderLogs(orderId);
   }
 }
