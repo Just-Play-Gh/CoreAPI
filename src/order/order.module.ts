f84f5ddbbb21e8bc1b19b3858c2a13e8ef55e3cf
David Yartey <20254688+dneey@users.noreply.github.com>
2021-12-28 15:02:43 +0000

    added http module

--- src/order/order.module.ts
+++ src/order/order.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { OrderService } from './order.service';
 import { OrderController } from './order.controller';
 import { SharedModule } from 'src/shared/shared.module';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
-  imports: [SharedModule],
+  imports: [SharedModule, HttpModule],
   providers: [OrderService],
   controllers: [OrderController],
 })
