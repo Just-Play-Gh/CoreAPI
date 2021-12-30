6eb61551bb3b060d1e3d9f005689bacd9f38d989
David Yartey <20254688+dneey@users.noreply.github.com>
2021-12-30 05:35:22 +0000

    removed unused code from gateway

--- src/app.gateway.ts
+++ src/app.gateway.ts
@@ -1,15 +1,5 @@
-import {
-  SubscribeMessage,
-  WebSocketGateway,
-  WebSocketServer,
-  MessageBody,
-  WsResponse,
-  ConnectedSocket,
-} from '@nestjs/websockets';
-import { from, Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
-
-import { Server, Socket } from 'socket.io';
+import { WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import { Server } from 'socket.io';
 
 @WebSocketGateway({
   cors: {
@@ -19,15 +9,4 @@ import { Server, Socket } from 'socket.io';
 export class AppGateway {
   @WebSocketServer()
   server: Server;
-
-  @SubscribeMessage('orders')
-  findAll(
-    @MessageBody() data: any,
-    @ConnectedSocket() client: Socket,
-  ): Observable<WsResponse<number>> {
-    console.log('we here and connected', data.driver === client.id);
-    return from([1, 2, 3]).pipe(
-      map((item) => ({ event: 'orders', data: item })),
-    );
-  }
 }
