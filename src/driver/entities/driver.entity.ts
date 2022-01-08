d4879c1847101cbfed772ffe8aae7b9ade69ce58
Komla Sodji <duhamelgking@gmail.com>
2022-01-08 17:19:03 +0000

    updates driver entity

--- src/driver/entities/driver.entity.ts
+++ src/driver/entities/driver.entity.ts
@@ -19,8 +19,11 @@ import { Order } from 'src/order/entities/order.entity';
 import { ReviewSummary } from 'src/reviews/review-summary/entities/review_summary.entity';
 
 export enum StatusType {
-  Active = '1',
-  Inactive = '0',
+  Active = 'active',
+  Disable = 'disable',
+  Busy = 'busy',
+  Online = 'Online',
+  Offline = 'offline',
 }
 
 @Entity({ name: 'drivers', schema: 'public' })
