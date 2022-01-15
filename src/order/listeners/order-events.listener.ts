d8b8f55b88e7176996897daa0e46d83051002bd0
David Yartey <20254688+dneey@users.noreply.github.com>
2022-01-15 14:57:18 +0000

    Order event name sand cleanup

--- src/order/listeners/order-events.listener.ts
+++ src/order/listeners/order-events.listener.ts
@@ -1,51 +1,62 @@
+import { InjectRedis, Redis } from '@nestjs-modules/ioredis';
 import { Injectable } from '@nestjs/common';
 import { OnEvent } from '@nestjs/event-emitter';
 import { AppGateway } from 'src/app.gateway';
 import { DriverService } from 'src/driver/driver.service';
 import { OrderAcceptedEvent } from '../events/order-accepted.event';
 import { OrderCreatedEvent } from '../events/order-created.event';
-import { PushOrderToDriverEvent } from '../events/push-order-to-driver-event';
+import { OrderEventNames } from '../order-event-names';
 
 @Injectable()
 export class OrderEventListeners {
+  orderAcceptedKey = 'order_accepted';
   constructor(
     private readonly appGateway: AppGateway,
     private readonly driverService: DriverService,
+    @InjectRedis() private readonly redis: Redis,
   ) {}
-  @OnEvent('order.created')
+
+  @OnEvent(OrderEventNames.Created)
   async handleOrderCreated(event: OrderCreatedEvent) {
+    console.log(event);
     const channelName = `${event.customerId}_order`;
-    const customerLocation = event.latlong;
     this.appGateway.server.emit(channelName, event);
-
-    const closestDrivers = await this.driverService.getClosestDriver(
-      customerLocation,
-    );
-    const sortedDistance = closestDrivers['sortedDistance'];
-    const sortedDriverIds = closestDrivers['sortedDrivers'];
-    sortedDistance.forEach(async (distance) => {
-      const channelName = sortedDriverIds[distance] + '_order';
-      this.appGateway.server.emit(channelName, event);
-    });
+    this.pushToDrivers(event);
+    return true;
   }
 
-  @OnEvent('order.pushToDriver')
-  handlePushOrderToDriverEvent(event: PushOrderToDriverEvent) {
-    console.log('push to driver event');
-    console.log(event.driverId);
-    const channelName = `${event.driverId}_order`;
-    console.log(channelName);
-    this.appGateway.server.emit(channelName, event);
-  }
-  @OnEvent('order.cancelled')
+  @OnEvent(OrderEventNames.Cancelled)
   handleOrderCancelledEvent(event) {
     const channelName = `${event.driverId}_order`;
     console.log(channelName);
     this.appGateway.server.emit(channelName, event);
   }
-  @OnEvent('order.accepted')
+  @OnEvent(OrderEventNames.Accepted)
   handleOrderAccepted(event: OrderAcceptedEvent) {
     this.appGateway.server.emit(`${event.customerId}_order`, event);
+    this.redis.set(this.orderAcceptedKey + event.orderId);
     console.log(event);
+  }
+
+  async pushToDrivers(event: OrderCreatedEvent) {
+    const customerLocation = event.latlong;
+    const closestDrivers = await this.driverService.getClosestDriver(
+      customerLocation,
+    );
+    const sortedDistance = closestDrivers['sortedDistance'];
+    const sortedDriverIds = closestDrivers['sortedDrivers'];
+    for (const distance of sortedDistance) {
+      const channelName = sortedDriverIds[distance] + '_order';
+      this.appGateway.server.emit(channelName, event);
+      await this.sleep(event.timeout);
+      if (this.redis.get(this.orderAcceptedKey + event.orderId)) {
+        console.log('Order accepted');
+        break;
+      }
+    }
+    // No one accepted
+  }
+  async sleep(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
   }
 }
