ec5f645b0f04993a2499505e6dee135be629e8e4
David Yartey <20254688+dneey@users.noreply.github.com>
2022-01-16 00:24:46 +0000

    added auth guards

--- src/truck/truck.controller.ts
+++ src/truck/truck.controller.ts
@@ -9,22 +9,26 @@ import {
   Query,
   DefaultValuePipe,
   ParseIntPipe,
+  UseGuards,
 } from '@nestjs/common';
 import { TruckService } from './truck.service';
 import { CreateTruckDto } from './dto/create-truck.dto';
 import { UpdateTruckDto } from './dto/update-truck.dto';
 import { GetOrderDto } from 'src/order/dto/get-order.dto';
+import { JwtAuthGuard } from 'src/guards/jwt-auth.guard';
 
 @Controller('trucks')
 export class TruckController {
   constructor(private readonly truckService: TruckService) {}
 
   @Post()
+  @UseGuards(JwtAuthGuard)
   create(@Body() createTruckDto: CreateTruckDto) {
     return this.truckService.create(createTruckDto);
   }
 
   @Get()
+  @UseGuards(JwtAuthGuard)
   async getTrucks(
     @Query('page', new DefaultValuePipe(1), ParseIntPipe) page = 1,
     @Query('limit', new DefaultValuePipe(15), ParseIntPipe) limit = 15,
@@ -36,16 +40,19 @@ export class TruckController {
   }
 
   @Get(':id')
+  @UseGuards(JwtAuthGuard)
   findOne(@Param('id') id: string) {
     return this.truckService.findOne(+id);
   }
 
   @Patch(':id')
+  @UseGuards(JwtAuthGuard)
   update(@Param('id') id: string, @Body() updateTruckDto: UpdateTruckDto) {
     return this.truckService.update(+id, updateTruckDto);
   }
 
   @Delete(':id')
+  @UseGuards(JwtAuthGuard)
   remove(@Param('id') id: string) {
     return this.truckService.remove(+id);
   }
