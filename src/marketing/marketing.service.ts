7b2b4bb47283f5fa708b9fc094c73e319e699cfd
David Yartey <20254688+dneey@users.noreply.github.com>
2022-01-16 02:12:41 +0000

    updates

--- src/marketing/marketing.service.ts
+++ src/marketing/marketing.service.ts
@@ -4,6 +4,7 @@ import {
   paginate,
   Pagination,
 } from 'nestjs-typeorm-paginate';
+import { AWSS3Service } from 'src/resources/aws-s3-service';
 import { User } from 'src/users/entities/user.entity';
 import { createQueryBuilder } from 'typeorm';
 import { CreateMarketingCampaignDto } from './dto/create-marketing-campaign.dto';
@@ -30,12 +31,15 @@ export class MarketingService {
   async create(
     createMarketingCampaignDto: CreateMarketingCampaignDto,
     user: User,
+    file,
   ) {
     try {
-      createMarketingCampaignDto.createdBy = user.id;
+      const imageUrl = await new AWSS3Service().uploadFile(file);
       const campaign = await MarketingCampaign.create(
         createMarketingCampaignDto,
       );
+      campaign.createdBy = user.id;
+      campaign.imageUrl = imageUrl;
       return await MarketingCampaign.save(campaign);
     } catch (error: any) {
       if (error.code === 'ER_DUP_ENTRY') {
@@ -61,6 +65,7 @@ export class MarketingService {
   async update(
     id: string,
     updateMarketingCampaignDto: UpdateMarketingCampaignDto,
+    file,
   ) {
     const campaign = await MarketingCampaign.findOne(id);
     if (!campaign)
@@ -68,9 +73,13 @@ export class MarketingService {
         'Marketing Campaign not found',
         HttpStatus.NOT_FOUND,
       );
+    if (updateMarketingCampaignDto.image) {
+      const imageUrl = await new AWSS3Service().uploadFile(file);
+      campaign.imageUrl = imageUrl;
+    }
     campaign.description = updateMarketingCampaignDto.description;
     campaign.name = updateMarketingCampaignDto.name;
-    campaign.image_url = updateMarketingCampaignDto.image;
+
     campaign.expiryDate = updateMarketingCampaignDto.expiryDate;
     campaign.scheduleDate = updateMarketingCampaignDto.scheduleDate;
     campaign.status = updateMarketingCampaignDto.status;
