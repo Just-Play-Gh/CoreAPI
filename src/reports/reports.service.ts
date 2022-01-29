65a01ec2951a07616b954bd42ff94303d5f513ae
Komla Sodji <duhamelgking@gmail.com>
2022-01-29 18:11:44 +0000

    update report

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -1,12 +1,21 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { paginate } from 'nestjs-typeorm-paginate';
 import { Order } from 'src/order/entities/order.entity';
+import { Between } from 'typeorm';
 
 @Injectable()
 export class ReportsService {
-  async orderReport(startDate, endDate) {
+  async orderReport(startDate, endDate, limit) {
     try {
-      const report = Order.find();
+      if (limit > +process.env.RECORD_LIMIT) {
+        return 'processing report ... it will be sent to you email shortly.';
+      }
+      const report = Order.find({
+        where: {
+          orderDate: Between(startDate.toISOString(), endDate.toISOString()),
+        },
+      });
+      return report;
     } catch (error) {
       Logger.error(error);
     }
