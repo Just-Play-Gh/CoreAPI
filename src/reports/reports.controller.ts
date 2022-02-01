e70ebb35897469f4242e203b863dfe74d9603aa8
Komla Sodji <duhamelgking@gmail.com>
2022-02-01 12:01:29 +0000

    updates

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,9 +1,12 @@
-import { Controller, Param } from '@nestjs/common';
+import { Controller, Param, UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from 'src/guards/jwt-auth.guard';
 import { ReportsService } from './reports.service';
 
 @Controller('reports')
 export class ReportsController {
   constructor(private readonly reportService: ReportsService) {}
+
+  @UseGuards(JwtAuthGuard)
   async orderReport(@Param() params) {
     return await this.reportService.orderReport(
       params.startDate,
