bf9ff1eac0692edf7e4a78ea94bedd0b8bc4b063
Komla Sodji <duhamelgking@gmail.com>
2022-02-01 11:55:31 +0000

    updates

--- src/dashboard/dashboard.controller.ts
+++ src/dashboard/dashboard.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from 'src/guards/jwt-auth.guard';
+import { DashboardService } from './dashboard.service';
 
 @Controller('dashboard')
-export class DashboardController {}
+export class DashboardController {
+  constructor(private readonly dashboardService: DashboardService) {}
+
+  @UseGuards(JwtAuthGuard)
+  async getAllUsers() {
+    return this.dashboardService.adminDashboard();
+  }
+}
