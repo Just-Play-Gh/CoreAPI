f8bc734f4dce06b420ecf7198f0b1a5e04b1dc11
David Yartey <20254688+dneey@users.noreply.github.com>
2022-05-14 11:56:02 +0000

    get roles

--- src/role/role.service.ts
+++ src/role/role.service.ts
@@ -1,4 +1,10 @@
-import { Injectable, Logger } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable, Logger } from '@nestjs/common';
+import {
+  IPaginationOptions,
+  paginate,
+  Pagination,
+} from 'nestjs-typeorm-paginate';
+import { createQueryBuilder, Not } from 'typeorm';
 import { BaseService } from '../resources/base.service';
 import { Role } from './entity/role.entity';
 
@@ -8,6 +14,37 @@ export class RoleService extends BaseService {
     super(Role);
   }
 
+  async getAll(
+    options: IPaginationOptions,
+    filter = {},
+  ): Promise<Pagination<Role>> {
+    const orderRepository = createQueryBuilder(Role, 'roles')
+      .where(filter)
+      .leftJoinAndSelect('roles.permissions', 'permissions') // Give me only roels with permissions
+      // .where('orders.driverId = drivers.id')
+      .orderBy({ 'roles.created': 'DESC' });
+
+    const orders = await paginate<Role>(orderRepository, options);
+    if (!orders['items'])
+      throw new HttpException('No orders were found', HttpStatus.NOT_FOUND);
+    return orders;
+  }
+
+  async createRoleWithPermission(
+    options: IPaginationOptions,
+    filter = {},
+  ): Promise<Pagination<Role>> {
+    const orderRepository = createQueryBuilder(Role, 'roles')
+      .where(filter)
+      .leftJoinAndSelect('roles.permissions', 'permissions')
+      // .where('orders.driverId = drivers.id')
+      .orderBy({ 'roles.created': 'DESC' });
+
+    const orders = await paginate<Role>(orderRepository, options);
+    if (!orders['items'])
+      throw new HttpException('No orders were found', HttpStatus.NOT_FOUND);
+    return orders;
+  }
   async roleByAlias(alias: string) {
     try {
       const role = await Role.findOne({ alias: alias });
