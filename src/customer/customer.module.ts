06242705d19ae86e07247529667037bd76923043
David Yartey <20254688+dneey@users.noreply.github.com>
2022-05-16 07:48:19 +0000

    fix on customer modul using auth service

--- src/customer/customer.module.ts
+++ src/customer/customer.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { CustomerController } from './customer.controller';
 import { CustomerService } from './customer.service';
 import { SharedModule } from 'src/shared/shared.module';
+import { AuthenticationService } from 'src/authentication/authentication.service';
 
 @Module({
   controllers: [CustomerController],
   exports: [CustomerService],
-  providers: [CustomerService],
+  providers: [CustomerService, AuthenticationService],
   imports: [CustomerModule, SharedModule],
 })
 export class CustomerModule {}
