ff9ed2481c793b32d5c428887522fe72befe4c58
David Yartey <20254688+dneey@users.noreply.github.com>
2022-05-16 07:34:13 +0000

    update docs

--- src/order/dto/create-order.dto.ts
+++ src/order/dto/create-order.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import {
   IsDateString,
   IsLatLong,
@@ -8,44 +9,58 @@ import { OrderDevice } from '../entities/order-devices.entity';
 
 export class CreateOrderDto {
   @IsOptional()
+  @ApiProperty()
   customerId: string;
 
   @IsOptional()
+  @ApiProperty()
   customerFullName: string;
 
   @IsOptional()
+  @ApiProperty()
   driverId: string;
 
   @IsOptional({ message: 'The amount field is required' })
+  @ApiProperty()
   amount: number;
 
-  @IsNotEmpty({ message: 'The device field is required' })
+  @IsOptional({ message: 'The device field is required' })
+  @ApiProperty()
   device: string;
 
   @IsNotEmpty({ message: 'The devices field is required' })
+  @ApiProperty()
   devices: [OrderDevice];
 
   @IsNotEmpty({ message: 'The address field is required' })
+  @ApiProperty()
   address: string;
 
   @IsNotEmpty({ message: 'The channel field is required' })
+  @ApiProperty()
   channel: string;
 
   @IsOptional()
+  @ApiProperty()
   currency: string;
 
   @IsNotEmpty({ message: 'The Product ID is required' })
+  @ApiProperty()
   productId: number;
 
   @IsOptional()
+  @ApiProperty()
   litres: number;
 
   @IsLatLong({ message: 'Customer latitude,longitude is required' })
+  @ApiProperty()
   latlong: string;
 
   @IsDateString()
+  @ApiProperty()
   scheduleDate: Date;
 
   @IsOptional()
+  @ApiProperty()
   customerLocation: string;
 }
