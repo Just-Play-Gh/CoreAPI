1a3cba9d80d607f39dc86c66960716b1d8c5929e
David Yartey <20254688+dneey@users.noreply.github.com>
2022-07-10 09:54:31 +0000

    custoemr servie update added paginate

--- src/customer/customer.service.ts
+++ src/customer/customer.service.ts
@@ -1,6 +1,11 @@
-import { Injectable, Logger } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable, Logger } from '@nestjs/common';
+import {
+  IPaginationOptions,
+  paginate,
+  Pagination,
+} from 'nestjs-typeorm-paginate';
 import { entityResource } from 'src/helpers/generator';
-import { Like } from 'typeorm';
+import { createQueryBuilder, Like } from 'typeorm';
 import { BaseService } from '../resources/base.service';
 import { Customer } from './entities/customer.entity';
 
@@ -9,7 +14,22 @@ export class CustomerService extends BaseService {
   constructor() {
     super(Customer);
   }
-
+  async paginate(
+    options: IPaginationOptions,
+    searchParams = {},
+  ): Promise<Pagination<Customer>> {
+    let customerRepository;
+    if (searchParams) {
+      customerRepository = createQueryBuilder(Customer).where(searchParams);
+      // .withDeleted();
+    } else {
+      customerRepository = createQueryBuilder(Customer).withDeleted();
+    }
+    const customers = await paginate<Customer>(customerRepository, options);
+    if (!customers['items'])
+      throw new HttpException('No customers were found', HttpStatus.NOT_FOUND);
+    return customers;
+  }
   async updateProfileDto(updateProfileDto, user: Customer) {
     const customer = await Customer.findOne({ id: user.id });
     for (const key in updateProfileDto) {
