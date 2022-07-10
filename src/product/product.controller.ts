b5bfa016e4ce7ee7c7a358cf5df9d451700a5527
David Yartey <20254688+dneey@users.noreply.github.com>
2022-07-10 09:21:43 +0000

    product controller updated findall

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,5 +1,14 @@
-import { Controller, Get, Query, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  DefaultValuePipe,
+  Get,
+  ParseIntPipe,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
 import { PermissionGuard } from 'src/guards/permission-guard';
+import { SearchUserDto } from 'src/users/dto/search-user.dto';
+import { Like } from 'typeorm';
 import { JwtAuthGuard } from '../guards/jwt-auth.guard';
 import { BaseController } from '../resources/base.controller';
 import { CreateProductDto } from './dto/create-product.dto';
@@ -12,6 +21,26 @@ export class ProductController extends BaseController {
     this.dtos = { store: CreateProductDto };
   }
 
+  @Get('/management')
+  findAll(
+    @Query('page', new DefaultValuePipe(1), ParseIntPipe) page = 1,
+    @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit = 10,
+    @Query() getProducts: SearchUserDto,
+  ) {
+    delete getProducts.page;
+    delete getProducts.limit;
+    let searchParams = {};
+
+    if (getProducts.searchField) {
+      searchParams = [
+        {
+          // [getProducts.searchField]: getProducts.searchValue,
+          name: Like('%' + getProducts.searchValue + '%'),
+        },
+      ];
+    }
+    return this.productService.paginate({ page, limit }, searchParams);
+  }
   @Get()
   @UseGuards(JwtAuthGuard)
   // @UseGuards(JwtAuthGuard, PermissionGuard)
