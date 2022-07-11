8832d46e12e8dc38aa6146b0319194801b2164ac
David Yartey <20254688+dneey@users.noreply.github.com>
2022-07-11 10:06:41 +0000

    Updates to create user dto, make many fields optional

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,16 +1,16 @@
 import { IsEmail, IsNotEmpty, IsOptional } from 'class-validator';
 
 export class CreateUserDto {
-  @IsNotEmpty({ message: 'Please enter a phone number' })
-  phoneNumber: string;
-
   @IsNotEmpty({ message: 'Please enter an email' })
   @IsEmail({ message: 'Please enter a valid email address' })
   email: string;
 
-  @IsNotEmpty({ message: 'Please enter a first name' })
+  @IsOptional({ message: 'Please enter a phone number' })
+  phoneNumber: string;
+
+  @IsOptional({ message: 'Please enter a first name' })
   firstName: string;
 
-  @IsNotEmpty({ message: 'Please enter a last name' })
+  @IsOptional({ message: 'Please enter a last name' })
   lastName: string;
 }
